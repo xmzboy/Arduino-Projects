4b306c01b58331c190be9d4c5ce60cecbee70e99
Ivan Andreichuk <vanichpz@gmail.com>
2022-06-05 14:01:09 +0300

    Update RTC.ino

--- RTC/RTC.ino
+++ RTC/RTC.ino
@@ -1,7 +1,7 @@
 #include <iarduino_RTC.h>
 
-#include <iarduino_RTC.h>
 iarduino_RTC time(RTC_DS1302);
+
 void setup() {
     delay(300);
     Serial.begin(9600);
@@ -11,9 +11,10 @@ void setup() {
     time.begin(a& , b& , c&);
     time.settime(0,51,21,27,10,15,2);  // 0  сек, 51 мин, 21 час, 27, октября, 2015 года, вторник
 }
+
 void loop(){
     if(millis()%1000==0){ // если прошла 1 секунда
-      Serial.println(time.gettime("d-m-Y, H:i:s, D")); // выводим время
-      delay(1); // приостанавливаем на 1 мс, чтоб не выводить время несколько раз за 1мс
+		Serial.println(time.gettime("d-m-Y, H:i:s, D")); // выводим время
+		delay(1); // приостанавливаем на 1 мс, чтоб не выводить время несколько раз за 1мс
     }
 }
