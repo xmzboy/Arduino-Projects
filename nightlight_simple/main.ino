7b44b4bc47b5ce39bb6776a3674fbd0f70d4d011
Ivan Andreichuk <vanichpz@gmail.com>
2022-06-05 14:00:43 +0300

    Update Nigthligth Project

--- nightlight_simple/main.ino
+++ nightlight_simple/main.ino
@@ -1,15 +1,14 @@
-int sensePin=0;
-int ledPin=9;
- void setup() {
- analogReference(DEFAULT);
- pinMode(ledPin,OUTPUT);
+int sensePin = 0;
+int ledPin = 9;
 
+void setup() {
+	analogReference(DEFAULT);
+	pinMode(ledPin,OUTPUT);
 }
 
 void loop() {
- int val=analogRead(sensePin);
- val=constrain(val,750,900);
- int ledLevel= map(val,750,900,255,0);
- analogWrite(ledPin,ledLevel);
-
+	int val=analogRead(sensePin);
+	val=constrain(val,750,900);
+	int ledLevel= map(val,750,900,255,0);
+	analogWrite(ledPin,ledLevel);
 }
